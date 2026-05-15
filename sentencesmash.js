6c62aa65a71681b7e320459ec56b45f1949c14b5
jmr623727 <jmrodriguezz2025@gmail.com>
2026-05-15 15:59:47 -0700

    Takes an Array of Strings and Smashes them together in JavaScript

---
+++ sentencesmash.js
@@ -0,0 +1,16 @@
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+// answer
+
+function smash (words) {
+    if(words.length === ''){
+      return ''
+    }
+    for(let i = 0; i <= words.length; i++){
+     return words.join(" ")
+    }
+  };
+  console.log(smash(['Jose', 'Rodriguez']))
