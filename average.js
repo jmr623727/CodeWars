fa5865d3972be254fbf7c08ede7a3297e3e1e47c
jmr623727 <161280206+jmr623727@users.noreply.github.com>
2026-05-15 15:22:29 -0700

    Update average.js

--- average.js
+++ average.js
@@ -4,8 +4,12 @@
 //answer
 function findAverage(array) {
   // your code here
-  return array.reduce((acc, c) => acc + c,0) / array.length
-  return 0;
+  if(!array.length){
+    return 0
+  }
+  average = array.reduce((acc, c) => acc + c,0) / array.length
+  return average
+  
 }
 
 findAverage([1,2,3])
