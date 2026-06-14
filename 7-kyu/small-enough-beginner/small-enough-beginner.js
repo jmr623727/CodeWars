897ebe195b978181ec3e260fa0c679fce2bd7272
jmr623727 <161280206+jmr623727@users.noreply.github.com>
2026-06-14 15:35:42 -0700

    Update small-enough-beginner.js

--- 7-kyu/small-enough-beginner/small-enough-beginner.js
+++ 7-kyu/small-enough-beginner/small-enough-beginner.js
@@ -1,4 +1,7 @@
 function smallEnough(a, limit){
   return Math.max(...a) <= limit
+
+  // alternative efficient for memory
+  // return a.every(x => x <= limit);
 }
 console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100))
